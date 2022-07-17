963955cc26fc8f0008b3136faaeeb93f8d5659a5
karolisjon <karolisjonn@gmail.com>
2022-07-17 18:38:59 +0300

    feat(9): created need subpages components

--- kj-woodwork/src/app.jsx
+++ kj-woodwork/src/app.jsx
@@ -1,11 +1,17 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import Navbar from './components/navbar/index';
+import HomePage from './components/pages/home-page';
+import MeetOurTeam from './components/pages/meet-our-team-page';
+import Products from './components/pages/products-page';
 
 const App = () => (
-      <Box>
+      <>
         <Navbar />
-      </Box>
+        <HomePage />
+        <MeetOurTeam />
+        <Products />
+      </>
   );
 
 export default App;

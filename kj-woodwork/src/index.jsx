55b215680d3661574d2d0898513a43762f0913db
karolisjon <karolisjonn@gmail.com>
2022-07-17 20:56:40 +0300

    feat(14): edited default theme color to black

--- kj-woodwork/src/index.jsx
+++ kj-woodwork/src/index.jsx
@@ -1,12 +1,15 @@
-import { CssBaseline } from '@mui/material';
 import * as React from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

edcf960e87d644673ccb781a8ca42d6fd9be7027
karolisjon <karolisjonn@gmail.com>
2022-07-24 16:21:43 +0300

    feat(25): added extra styles in CustomButton component

--- kj-woodwork/src/components/custom-button.jsx
+++ kj-woodwork/src/components/custom-button.jsx
@@ -1,14 +1,21 @@
 import { Button, styled } from '@mui/material'; 
 
-const CustomButton = styled(Button)({
+const CustomButton = styled(Button)(({ theme }) => ({
   width: '300px',
   height: '50px',
+  mt: 2,
+  mb: 4,
   backgroundColor: '#000000b8',
-  color: '#fff',
   borderRadius: '0',
   border: '2px solid #fff',
-  mt: 2,
-  mb: 2,
-});
+  color: '#fff',
+  letterSpacing: '0.1em',
+  fontSize: '14px',
+  transition: '0.3s ease-in-out',
+
+  ':hover': {
+    backgroundColor: theme.palette.primary.dark,
+  }
+}));
 
 export default CustomButton;
--- kj-woodwork/src/components/pages/home-page/index.jsx
+++ kj-woodwork/src/components/pages/home-page/index.jsx
@@ -2,7 +2,7 @@ import {
   Box,
   Typography,
   Container,
-  Divider
+  // Divider
 } from '@mui/material';
 import React from 'react';
 import CustomButton from '../../custom-button';
@@ -29,21 +29,35 @@ const HomePage = () => {
           <Typography
             maxWidth='sm'
             paragraph
-            sx={{ color: 'white', fontWeight: '400', mt: 2 }}>
+            sx={{ color: 'white', 
+            fontWeight: '400',
+            letterSpacing: '0.1em', 
+            mt: 2,
+            backgroundColor: '#000000b8',
+            p: 2,
+            }}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Illum hic, consequuntur est ipsa ex cumque exercitationem amet?
             Quia modi quae alias recusandae aperiam atque nesciunt blanditiis enim iusto ut totam
             ab debitis nostrum praesentium maxime.
           </Typography>
-          <Divider sx={{ backgroundColor: '#fff', height: '2px', width: '20%',}} />
+          <CustomButton>Meet our team</CustomButton>
+          {/* <Divider sx={{ backgroundColor: '#fff', height: '3px', width: '20%', }} /> */}
           <Typography
             maxWidth='sm'
             paragraph
-            sx={{ color: 'white', fontWeight: '400', mt: 2 }}>
+            sx={{ 
+              color: 'white', 
+              fontWeight: '400', 
+              letterSpacing: '0.1em',
+              mt: 2,
+              backgroundColor: '#000000b8',
+              p: 2
+              }}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque perferendis illo tempora officiis maiores quidem minima velit, possimus quaerat, iusto, sed nesciunt ipsam labore dignissimos.
           </Typography>
           <CustomButton>Products</CustomButton>
-          <Divider sx={{ backgroundColor: '#fff', height: '2px', width: '20%', mt: 2 }} />
+          {/* <Divider sx={{ backgroundColor: '#fff', height: '3px', width: '20%', mt: 2 }} /> */}
         </Container>
       </ContentContainer>
     </Box>

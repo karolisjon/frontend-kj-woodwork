95cc3226cbe4a4a4899f77b91a1649483683243a
karolisjon <karolisjonn@gmail.com>
2022-07-26 21:30:08 +0300

    feat(38): created CartPage component and added routing to it

--- kj-woodwork/src/app.jsx
+++ kj-woodwork/src/app.jsx
@@ -10,6 +10,7 @@ import MeetOurTeam from './components/pages/meet-our-team-page';
 import Products from './components/pages/products-page';
 import OrderPage from './components/pages/order-page';
 import CartContext from './contexts/cart-page-context';
+import CartPage from './components/pages/cart-page';
 
 const App = () => (
   <BrowserRouter>
@@ -20,6 +21,7 @@ const App = () => (
         <Route path='/meet-our-team' element={<MeetOurTeam />} />
         <Route path='/products' element={<Products />} />
         <Route path='/order' element={<OrderPage />} />
+        <Route path='/cart' element={<CartPage />} />
       </Routes>
     </CartContext.Provider>
   </BrowserRouter>
---
+++ kj-woodwork/src/components/pages/cart-page/index.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const CartPage = () => {
+  return (
+    <div>CartPage</div>
+  )
+}
+
+export default CartPage;

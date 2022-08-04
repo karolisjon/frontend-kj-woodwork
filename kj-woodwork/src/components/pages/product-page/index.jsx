42cd5717f0e56f292dec8a8939ffda322ed4ed40
karolisjon <karolisjonn@gmail.com>
2022-08-04 14:33:33 +0300

    feat(59): started creating design for ProductInformation component

--- kj-woodwork/src/components/pages/product-page/index.jsx
+++ kj-woodwork/src/components/pages/product-page/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Container } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Box, Button, Divider, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 const ProductInformation = () => {
@@ -20,13 +20,126 @@ const ProductInformation = () => {
 
   }, [id]);
 
+  //console.log({ product: product.title });
+
   return (
     <Container>
       <Typography variant='h4' component='h4'>Product information {id}</Typography>
-      <pre>
-        {JSON.stringify(product, null, 4)}
-      </pre>
-    </Container>
+
+
+
+      <Box sx={{ display: 'flex', mb: 4 }}>
+        <Box>
+          <Box
+            component='img'
+            src='https://www.arlberry.com/wp-content/uploads/2014/01/Mia-Marquez-1-744x496.jpg'
+            alt=''
+            sx={{
+              width: '600px',
+              objectFit: 'cover',
+            }}
+          >
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            // px: 4, 
+            py: 1,
+            flexGrow: 1
+          }}>
+            <Typography variant='h5' component='h6'>Dining table</Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>Furniture</Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>Type of wood: Maple</Typography>
+            <Typography component='p' sx={{ my: 2 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Placeat commodi quia dicta suscipit impedit possimus eum doloribus iste ab,
+              veniam mollitia iusto deleniti qui, magnam voluptate harum aliquam quidem enim quas!
+              Esse mollitia nesciunt quod consequatur culpa nostrum! Repudiandae quam in modi
+              sequi voluptas velit blanditiis fugiat ipsa? At, magnam.
+              <Divider sx={{ my: 1 }}></Divider>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Placeat commodi quia dicta suscipit impedit possimus eum doloribus iste ab,
+              veniam mollitia iusto deleniti qui, magnam voluptate harum aliquam quidem enim quas!
+            </Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>569 EUR</Typography>
+            <Button sx={theme => ({
+              width: '100%',
+              height: '50px',
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.white,
+              borderRadius: '0',
+              letterSpacing: '0.1em',
+              fontSize: '14px',
+              transition: '0.3s ease-in-out',
+            })}>Add to cart</Button>
+          </Box>
+        </Box>
+      </Box>
+
+
+
+
+      <Box sx={{ display: 'flex', mb: 4 }}>
+        <Box>
+          <Box
+            component='img'
+            src='https://www.wright20.com/items/index/2000/157_1_modern_design_march_2011_sam_maloof_dining_chairs_pair__wright_auction.jpg'
+            alt=''
+            sx={{
+              width: '600px',
+              objectFit: 'cover',
+            }}
+          >
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            // px: 4, 
+            py: 1,
+            flexGrow: 1
+          }}>
+            <Typography variant='h5' component='h6'>Dining table</Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>Furniture</Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>Type of wood: Maple</Typography>
+            <Typography component='p' sx={{ my: 2 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Placeat commodi quia dicta suscipit impedit possimus eum doloribus iste ab,
+              veniam mollitia iusto deleniti qui, magnam voluptate harum aliquam quidem enim quas!
+              Esse mollitia nesciunt quod consequatur culpa nostrum! Repudiandae quam in modi
+              sequi voluptas velit blanditiis fugiat ipsa? At, magnam.
+              <Divider sx={{ my: 1 }}></Divider>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Placeat commodi quia dicta suscipit impedit possimus eum doloribus iste ab,
+              veniam mollitia iusto deleniti qui, magnam voluptate harum aliquam quidem enim quas!
+            </Typography>
+            <Typography component='subtitle' sx={{ my: 1 }}>569 EUR</Typography>
+            <Button sx={theme => ({
+              width: '100%',
+              height: '50px',
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.white,
+              borderRadius: '0',
+              letterSpacing: '0.1em',
+              fontSize: '14px',
+              transition: '0.3s ease-in-out',
+            })}>Add to cart</Button>
+          </Box>
+        </Box>
+      </Box>
+
+
+
+
+
+
+
+
+      <Box sx={{ mt: 30 }}>
+        <pre>
+          {JSON.stringify(product, null, 4)}
+        </pre>
+      </Box>
+    </Container >
   )
 }
 

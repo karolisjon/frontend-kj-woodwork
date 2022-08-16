90364030fd5099d24fd37904f2fb6788f2e8e8bd
karolisjon <karolisjonn@gmail.com>
2022-08-16 21:56:06 +0300

    feat(75): adjusted styles for AuthLayout component

--- kj-woodwork/src/layouts/auth-layout.jsx
+++ kj-woodwork/src/layouts/auth-layout.jsx
@@ -1,14 +1,20 @@
 import { Box } from '@mui/material';
+import { Container } from '@mui/system';
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/navbar';
 
 const AuthLayout = () => {
   return (
-    <Box>
-      <Navbar />
-      <Outlet />
-    </Box>
+    <Container>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Container>
   )
 }
 

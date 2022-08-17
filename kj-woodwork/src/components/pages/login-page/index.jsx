e631393524cb621697dc341da7ccf80922866c75
karolisjon <karolisjonn@gmail.com>
2022-08-17 21:47:07 +0300

    feat(77): created form design in LoginPage component

--- kj-woodwork/src/components/pages/login-page/index.jsx
+++ kj-woodwork/src/components/pages/login-page/index.jsx
@@ -1,8 +1,49 @@
+import { Box, Link, Paper, TextField, Typography } from '@mui/material';
 import * as React from 'react';
+import CustomButton from '../../../custom-button';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Paper sx={{
+      width: '400px',
+      borderRadius: 0,
+      mt: 4,
+      py: 6,
+      px: 4,
+    }}>
+      <Box sx={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        gap: 2,
+        }}>
+        <Typography component='h1' variant='h3' sx={{ fontFamily: 'logoFont', fontWeight: 500 }}>KJ Woodwork</Typography>
+        <Typography component='h2' variant='h5' sx={{ textTransform: 'uppercase' }}>Login</Typography>
+      </Box>
+      <Box 
+      component='form' 
+      sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+      >
+        <TextField
+          variant='standard'
+          label='Email'
+          type='email'
+          fullWidth
+        />
+        <TextField
+          variant='standard'
+          label='Password'
+          type='password'
+          fullWidth
+        />
+        <Link sx={{ fontSize: '12px' }}>Forgot your password?</Link>
+        <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
+          <Typography sx={{ fontSize: '12px' }}>Don&apos;t have an account?</Typography>
+          <Link sx={{ fontSize: '12px' }}>Sign up</Link>
+        </Box>
+        <CustomButton>LOGIN</CustomButton>
+      </Box>
+    </Paper>
   )
 }
 

91e3957acd1bb24c65f4f4cd928adbacc8b43459
karolisjon <karolisjonn@gmail.com>
2022-08-18 13:50:30 +0300

    feat(82): adjusted form validation to show error messages on specific conditions

--- kj-woodwork/src/components/pages/login-page/index.jsx
+++ kj-woodwork/src/components/pages/login-page/index.jsx
@@ -11,26 +11,26 @@ const initialVals = {
 
 const LoginPage = () => {
   let navigate = useNavigate();
-  const { 
-    values, dirty, errors, isValid,
-    handleChange,
+  const {
+    values, dirty, errors, isValid, touched,
+    handleChange, handleBlur,
   } = useFormik({
     initialValues: initialVals,
     validate: ({ email, password }) => {
       let fieldErrors = {};
-      
+
       if (email === '') {
         fieldErrors.email = 'Email is mandatory';
       }
 
-      if (password === '') { 
+      if (password === '') {
         fieldErrors.password = 'Password is mandatory';
       }
 
-    return fieldErrors;
+      return fieldErrors;
     }
   });
-  
+
   return (
     <Paper sx={{
       width: '400px',
@@ -40,21 +40,26 @@ const LoginPage = () => {
       px: 4,
     }}>
       <Paper component='pre' sx={{ position: 'fixed', top: '80px', left: '50px', p: 2 }}>
-        {JSON.stringify({ values, dirty, errors }, null, 2)}
+        {JSON.stringify({
+          values,
+          dirty,
+          errors,
+          touched
+        }, null, 2)}
       </Paper>
 
-      <Box sx={{ 
-        display: 'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         gap: 2,
-        }}>
+      }}>
         <Typography component='h1' variant='h3' sx={{ fontFamily: 'logoFont', fontWeight: 500 }}>KJ Woodwork</Typography>
         <Typography component='h2' variant='h5' sx={{ textTransform: 'uppercase' }}>Login</Typography>
       </Box>
-      <Box 
-      component='form' 
-      sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+      <Box
+        component='form'
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
       >
         <TextField
           name='email'
@@ -62,9 +67,10 @@ const LoginPage = () => {
           variant='standard'
           type='email'
           fullWidth
+          error={touched.email && Boolean(errors.email)}
+          helperText={touched.email && errors.email}
           onChange={handleChange}
-          error={Boolean(errors.email)}
-          helperText={errors.email}
+          onBlur={handleBlur}
         />
         <TextField
           name='password'
@@ -72,36 +78,37 @@ const LoginPage = () => {
           variant='standard'
           type='password'
           fullWidth
+          error={touched.password && Boolean(errors.password)}
+          helperText={touched.password && errors.password}
           onChange={handleChange}
-          error={Boolean(errors.password)}
-          helperText={errors.password}
+          onBlur={handleBlur}
         />
-        <Link 
-        variant='body1'
-        sx={{ 
-          fontSize: '12px',
-          transition: '0.3s ease-in-out',
-          '&:hover': {
-            cursor: 'pointer'
-          } 
-           }}>
-            Forgot your password?
-            </Link>
-        <Box sx={{ display: 'flex', gap: 0.5, mb: 2 }}>
-          <Typography variant='body1'sx={{ fontSize: '12px' }}>Don&apos;t have an account?</Typography>
-          <Link 
+        <Link
           variant='body1'
-          sx={{ 
+          sx={{
             fontSize: '12px',
             transition: '0.3s ease-in-out',
+            '&:hover': {
+              cursor: 'pointer'
+            }
+          }}>
+          Forgot your password?
+        </Link>
+        <Box sx={{ display: 'flex', gap: 0.5, mb: 2 }}>
+          <Typography variant='body1' sx={{ fontSize: '12px' }}>Don&apos;t have an account?</Typography>
+          <Link
+            variant='body1'
+            sx={{
+              fontSize: '12px',
+              transition: '0.3s ease-in-out',
               '&:hover': {
                 cursor: 'pointer'
-              } 
+              }
             }}
             onClick={() => navigate('/register')}
-            >
-              Sign up
-              </Link>
+          >
+            Sign up
+          </Link>
         </Box>
         <CustomButton disabled={!dirty || !isValid}>LOGIN</CustomButton>
       </Box>

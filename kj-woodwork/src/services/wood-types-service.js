46c5ac24f0ffd462b2541969a8b5905b925bf5ca
karolisjon <karolisjonn@gmail.com>
2022-08-25 14:53:33 +0300

    feat(109): created wood-types-service and updated data in database.json

---
+++ kj-woodwork/src/services/wood-types-service.js
@@ -0,0 +1,14 @@
+const domain = 'http://localhost:8000/';
+
+const fetchWoodTypes = async () => {
+  const response = await fetch(`${domain}/woodtypes`);
+  const woodTypes = await response.json();
+
+  return woodTypes;
+};
+
+const WoodTypeService = {
+  fetchWoodTypes,
+};
+
+export default WoodTypeService;

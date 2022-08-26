bf847a4aac4107d372b7c2b5c5fb45abc5a343e5
karolisjon <karolisjonn@gmail.com>
2022-08-26 13:43:49 +0300

    feat(120): created relationsParams variable in product-service

--- kj-woodwork/src/services/product-service.js
+++ kj-woodwork/src/services/product-service.js
@@ -1,8 +1,9 @@
 const domain = 'http://localhost:8000';
 const databaseCollection = 'products';
+const relationsParams = '_expand=category&_expand=woodType';
 
 const fetchProducts = async () => {
-  const response = await fetch(`${domain}/${databaseCollection}/?_expand=category&_expand=woodType`);
+  const response = await fetch(`${domain}/${databaseCollection}/?${relationsParams}`);
   const products = await response.json();
 
   return products;

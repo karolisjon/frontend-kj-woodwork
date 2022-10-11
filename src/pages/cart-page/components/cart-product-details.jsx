eeb1230c88ba85cd0b6d0525f31e4824d1b5ffc1
karolisjon <karolisjonn@gmail.com>
2022-10-11 21:55:29 +0300

    feat(170): responsive design adjustments in CartProductDetails component

--- src/pages/cart-page/components/cart-product-details.jsx
+++ src/pages/cart-page/components/cart-product-details.jsx
@@ -12,13 +12,13 @@ const CartProductDetails = ({ productsInCart }) => (
   <Box sx={{
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'space-between',
     my: 2,
   }}
   >
     <Box sx={{
       display: 'flex',
       flexDirection: 'row',
-      width: '70%',
     }}
     >
       <img src={productsInCart.img} alt="" height="260px" width="260px" />
@@ -31,66 +31,78 @@ const CartProductDetails = ({ productsInCart }) => (
           {productsInCart?.title}
         </Typography>
         {/* <Typography
-      variant="body2"
-      component="p"
-      sx={{  fontFamily: theme.typography.main, my: 1 }}>
-        Category:
-        {' '}
-        {productsInCart?.category.title}
-      </Typography>
-      <Typography
-      variant="body2"
-      component="p"
-      sx={{  fontFamily: theme.typography.main, my: 1 }}>
-        Type of wood:
-        {' '}
-        {productsInCart?.woodType.title}
-      </Typography> */}
+          variant="body2"
+          component="p"
+          sx={{ fontFamily: theme.typography.main, my: 1 }}
+        >
+          Category:
+          {' '}
+          {productsInCart?.category.title}
+        </Typography>
+        <Typography
+          variant="body2"
+          component="p"
+          sx={{ fontFamily: theme.typography.main, my: 1 }}
+        >
+          Type of wood:
+          {' '}
+          {productsInCart?.woodType.title}
+        </Typography> */}
       </Box>
     </Box>
 
     <Box sx={{
       display: 'flex',
-      flexDirection: 'row',
+      flexDirection: 'column',
       justifyContent: 'space-between',
-      width: '30%',
+      width: { md: '30%' },
 
     }}
     >
-      <Box>
-        <Typography sx={{ fontFamily: theme.typography.main }}>QUANTITY: </Typography>
-        <Typography sx={{ fontFamily: theme.typography.main }}>2</Typography>
-      </Box>
-      <Box>
-        <Typography sx={{
-          fontFamily: theme.typography.main,
-
-        }}
-        >
-          PRICE:
-        </Typography>
-        <Typography sx={{ fontFamily: theme.typography.main }}>
-          {productsInCart.price}
-          {' Є'}
-        </Typography>
-      </Box>
-      <Box>
-        <Typography sx={{ fontFamily: theme.typography.main }}>TOTAL: </Typography>
-        <Typography sx={{ fontFamily: theme.typography.main }}>
-          {productsInCart.price}
-          {' Є'}
-        </Typography>
-      </Box>
-    </Box>
-
-    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <IconButton
-        sx={{ color: theme.palette.primary.main, '&:hover': { background: 'none' } }}
-        onClick={() => console.log('will remove item from the cart in the future')}
+      <Box sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+        justifyContent: 'space-between',
+        gap: { xs: 3 },
+      }}
       >
-        <ClearIcon />
-      </IconButton>
+        <Box>
+          <Typography sx={{ fontFamily: theme.typography.main }}>QUANTITY: </Typography>
+          <Typography sx={{ fontFamily: theme.typography.main }}>2</Typography>
+        </Box>
+        <Box>
+          <Typography sx={{
+            fontFamily: theme.typography.main,
+
+          }}
+          >
+            PRICE:
+          </Typography>
+          <Typography sx={{ fontFamily: theme.typography.main }}>
+            {productsInCart.price}
+            {' Є'}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography sx={{ fontFamily: theme.typography.main }}>TOTAL: </Typography>
+          <Typography sx={{ fontFamily: theme.typography.main }}>
+            {productsInCart.price}
+            {' Є'}
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <IconButton
+          sx={{ color: theme.palette.primary.main, '&:hover': { background: 'none' } }}
+          onClick={() => console.log('will remove item from the cart in the future')}
+        >
+          <ClearIcon />
+        </IconButton>
+      </Box>
+
     </Box>
+
   </Box>
 );
 
